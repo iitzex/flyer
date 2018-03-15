bc9ef66ca71526c3f750dbd30a6e829d9d9e8ed2
iitze <iitzex@gmail.com>
2018-03-15 08:30:59 +0800

    精準計算出過每個checkpoint的時間

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
-from json import JSONDecodeError
 import math
+import os
 import time
+from json import JSONDecodeError
 from os import listdir
-from os.path import isfile, join
 
 from plot import bokeh_draw
 
@@ -13,6 +13,11 @@ N7 = (25.084587, 121.232751, 'N7', '=$')
 N8 = (25.087269, 121.235753, 'N8', '=$')
 N9 = (25.089568, 121.238610, 'N9', '=$')
 N10 = (25.092774, 121.242751, 'N10', '=$')
+Start = (R05L)
+Exit = (N5, N7, N8, N9, N10)
+
+lookup = {}
+traffic = []
 
 
 def runtime():
@@ -28,14 +33,36 @@ def runtime():
                 pass
 
             checkpoint(j['aircraft'], fn)
-            # draw(j['aircraft'], fn)
+    
+    for f in traffic:
+        print(f[0], f[2], f[3])
 
 
-def dist(p, cs, lat, lon, scale, fn, f):
+def dist(p, scale, f, fn):
+    cs = f['flight']
+    lat = f['lat']
+    lon = f['lon']
+
     d = pow(10000 * (p[0] - lat), 2) + pow(10000 * (p[1] - lon), 2)
     if d < scale:
-        print(cs, d, fn, p[2], p[3])
-        print(f)
+        # print(cs, d, fn, p, symbol, sep=',')
+        k = cs+p[3]
+        if  k not in lookup:
+            lookup[k] = d
+            traffic.append((cs, d, fn, p[2], p[3]))
+            # print('-', lookup)
+            # print('-', traffic)
+        elif d < lookup[k]:
+            lookup[k] = d
+            del traffic[-1]
+            # print('_________')
+            # print('+', lookup)
+            # print('+', traffic)
+            traffic.append((cs, d, fn, p[2], p[3]))
+
+        return (cs, d, fn, p[2], p[3])
+    else:
+        return (cs, -1, None, None, None)
 
 
 def checkpoint(aircraft, fn):
@@ -46,20 +73,11 @@ def checkpoint(aircraft, fn):
         except KeyError:
             continue
 
-        cs = f['flight']
-        lat = f['lat']
-        lon = f['lon']
+        # cs = f['flight']
 
-        dist(R05L, cs, lat, lon, 30, fn, f)
-        dist(N5, cs, lat, lon, 5, fn, f)
-        dist(N7, cs, lat, lon, 5, fn, f)
-        dist(N8, cs, lat, lon, 5, fn, f)
-        dist(N8, cs, lat, lon, 5, fn, f)
-        dist(N9, cs, lat, lon, 5, fn, f)
-        dist(N10, cs, lat, lon, 5, fn, f)
-
-    # print('.', end='')
-    # time.sleep(0.1)
+        dist(R05L, 30, f, fn)
+        for e in Exit:
+            dist(e, 5, f, fn)
 
 
 def draw(aircraft, fn):
